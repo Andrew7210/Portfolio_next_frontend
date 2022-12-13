22c589ee6ca1e3deaa5d80c4d7bb186acbc3737d
Tianyi.Zhan <tianyi.zhan@outlook.com>
2022-12-13 11:24:14 +0800

    Initial commit from Create Next App

---
+++ src/lib/sanity.client.ts
@@ -0,0 +1,15 @@
+// ./src/lib/sanity.client.ts
+
+import {createClient} from 'next-sanity'
+import imageUrlBuilder from '@sanity/image-url'
+
+export const config = {
+  projectId : process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset : process.env.NEXT_PUBLIC_SANITY_DATASET,
+  apiVersion : process.env.NEXT_PUBLIC_SANITY_API_VERSION,
+  useCdn: true,
+  sanityToken : process.env.SANITY_API_TOKEN
+}
+
+export const client = createClient(config)
+export const urlFor = (source) => imageUrlBuilder(client).image(source)
---
+++ src/pages/test.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import HolwsCastle from '../components/Characters/HolwsCastle'
+function test() {
+  return (
+    <HolwsCastle />
+  )
+}
+
+export default test
