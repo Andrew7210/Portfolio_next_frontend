b5c042cee521e6015f0108a6f6cc2bacb074488b
Tianyi.Zhan <tianyi.zhan@outlook.com>
2023-01-21 14:17:03 +0800

    add loading page to project section

---
+++ src/components/Screens/index.js
@@ -0,0 +1,8 @@
+import Introduce from './Introduce'
+import Education from './Education'
+import Learning from './Learning'
+import WorkExperience from './WorkExperience'
+import Network from './Network'
+import IntroToGame from './IntroToGame'
+import SuperMarioScene from './SuperMarioScene'
+export {Introduce, Education, Learning, WorkExperience, Network, IntroToGame, SuperMarioScene};
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Spline from '@splinetool/react-spline'
 import Lottie from "lottie-react";
 import linkedin from "public/lotti/linkedin.json";
 import github from "public/lotti/gitcat.json";
+import developer from "public/lotti/developer.json";
 import { useRive, useStateMachineInput } from "@rive-app/react-canvas";
-import {motion} from 'framer-motion'
-import Mywork from './Mywork';
+import {motion, AnimatePresence} from 'framer-motion'
+import Mywork from '../components/Mywork';
+import { Typewriter } from 'react-simple-typewriter'
 
 const Projects = () => {
+  const [showScreen, setShowScreen] = useState(true)
   const { rive, RiveComponent} = useRive({
     src: "rive/pigeons_button.riv",
     stateMachines: "State Machine 1",
@@ -15,13 +18,33 @@ const Projects = () => {
   });
   return (
     <>
+      <AnimatePresence>
+        {showScreen && (
+          <motion.div className="w-screen h-screen fixed top-0 z-30 bg-[#140e20] flex flex-col justify-center items-center text-center" exit={{opacity:0}}>
+            <div class="bookshelf_wrapper">
+              <ul class="books_list">
+                <li class="book_item first"></li>
+                <li class="book_item second"></li>
+                <li class="book_item third"></li>
+                <li class="book_item fourth"></li>
+                <li class="book_item fifth"></li>
+                <li class="book_item sixth"></li>
+              </ul>
+              <div class="shelf"></div>
+            </div>
+            <h2 className='p-4 text-4xl font-bold text-purple-600 relative -bottom-[20%]'>Loading...</h2>
+          </motion.div>
+        )}
+      </AnimatePresence> 
       <div className='flex flex-col'>
-        <div className='flex flex-row h-screen'>
-          <div className='absolute top-0 right-0 w-full h-full m-0 '><Spline scene="https://prod.spline.design/HJXKk3Al5odruWDq/scene.splinecode" /></div>
+        <div className='flex flex-row h-screen snap-start'>
+          <div className='absolute top-0 right-0 w-full h-full m-0 '><Spline scene="https://prod.spline.design/HJXKk3Al5odruWDq/scene.splinecode" onLoad={()=>setTimeout(() => {
+            setShowScreen(false)
+          }, 200)}/></div>
           <div className='flex flex-col justify-center pl-[10vw] z-10 pointer-events-none'>
-            <h2 className='p-4 text-2xl font-bold text-purple-600 fill-white'>What was impossible, Technology makes possible</h2>
-            <h1 className='max-w-5xl p-6 font-extrabold text-white fill-white text-8xl'>Time to check the power of knowledge </h1>
-            <h2 className='max-w-2xl p-4 text-4xl font-bold text-white fill-white'>The best way to predict the future is to learn and create it.</h2>
+            <h2 className='p-4 text-2xl font-bold text-purple-600 '>What was impossible, Technology makes possible</h2>
+            <h1 className='max-w-5xl p-6 font-extrabold text-white text-8xl'>Time to check the power of knowledge </h1>
+            <h2 className='max-w-2xl p-4 text-4xl font-bold text-white '>The best way to predict the future is to learn and create it.</h2>
             
             <div className='flex flex-row mt-6'>
               <RiveComponent className="mr-32 relative -top-56 w-[300px] h-[400px] pointer-events-auto" />
